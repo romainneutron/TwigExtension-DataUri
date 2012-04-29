a100f5b99d0b818bfe47a90885569860699298ff
Romain Neutron <imprec@gmail.com>
2012-04-29 23:48:55 +0200

    Initial import

---
+++ src/DataUri/TwigExtension.php
@@ -0,0 +1,122 @@
+<?php
+
+/**
+ * This tig extension is released under MIT license
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace DataURI;
+
+/**
+ * Twig extension for data URI, see README for example of use
+ * Converts datas to the data URI Url scheme
+ *
+ * @see https://www.ietf.org/rfc/rfc2397.txt
+ */
+class TwigExtension extends \Twig_Extension
+{
+
+    /**
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'data_uri_twig_extension';
+    }
+
+    /**
+     *
+     * @return type
+     */
+    public function getFilters()
+    {
+        return array(
+            'dataUri' => new \Twig_Filter_Method($this, 'dataUri'),
+        );
+    }
+
+    /**
+     *
+     * @param mixed     $source     DataURI source
+     * @param boolean   $strict     Use strict mode (length output)
+     * @param string    $mime       the mime type
+     * @param array     $parameters Extra parameters, see rfc
+     * @return null
+     */
+    public function dataUri($source, $strict = true, $mime = null, $parameters = array())
+    {
+        $data = null;
+
+        try {
+            switch (true) {
+                case is_resource($source):
+
+                    $data = $this->getDataFromRessource($source, $strict, $mime, $parameters);
+
+                    break;
+                case is_scalar($source):
+
+                    $data = $this->getDataFromScalar($source, $strict, $mime, $parameters);
+
+                    break;
+                default:
+                    trigger_error("Tried to convert an unsupported source format", E_USER_WARNING);
+                    break;
+            }
+        } catch (Exception\Exception $e) {
+
+            trigger_error(sprintf("Error while building DataUri : %s", $e->getMessage()), E_USER_WARNING);
+        }
+
+        if ($data) {
+
+            return Dumper::dump($data);
+        }
+
+        return null;
+    }
+
+    /**
+     *
+     * @param ressource     $source
+     * @param boolean       $strict
+     * @param string        $mime
+     * @param array         $parameters
+     * @return \DataURI\Data
+     */
+    protected function getDataFromRessource($source, $strict, $mime, Array $parameters)
+    {
+
+        $streamDatas = null;
+
+        while ( ! feof($source)) {
+            $streamDatas .= fread($source, 8192);
+        }
+
+        $data =  new Data($streamDatas, $mime, $parameters, $strict);
+        $data->setBinaryData(true);
+
+        return $data;
+    }
+
+    /**
+     *
+     * @param string        $source
+     * @param boolean       $strict
+     * @param string        $mime
+     * @param array         $parameters
+     * @return \DataURI\Data
+     */
+    protected function getDataFromScalar($source, $strict, $mime, $parameters)
+    {
+
+        if (file_exists($source)) {
+            return Data::buildFromFile($source, true, $strict);
+        }
+
+        return new Data($source, $mime, $parameters, $strict);
+    }
+}
---
+++ tests/src/DataUri/TwigExtensionTest.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace DataURI;
+
+class TwigExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Twig_Environment
+     */
+    protected $twig;
+
+    protected function setUp()
+    {
+        $loader = new \Twig_Loader_String();
+        $this->extension = new TwigExtension();
+        $this->twig = new \Twig_Environment($loader);
+        $this->twig->addExtension($this->extension);
+    }
+
+    /**
+     * @covers DataUri\TwigExtension::getName
+     */
+    public function testGetName()
+    {
+        $this->assertEquals('data_uri_twig_extension', $this->extension->getName());
+    }
+
+    /**
+     * @covers DataUri\TwigExtension::getFilters
+     */
+    public function testGetFilters()
+    {
+        $filters = $this->extension->getFilters();
+        $this->assertArrayHasKey('dataUri', $filters);
+        $method = $filters['dataUri'];
+        $this->assertInstanceOf('\\Twig_Filter_Method', $method);
+    }
+
+    /**
+     * @covers DataUri\TwigExtension::dataUri
+     * @covers DataUri\TwigExtension::getDataFromRessource
+     */
+    public function testDataUriRessource()
+    {
+        $file = __DIR__ . '/../../smile.png';
+        $ressource = fopen($file, 'r');
+
+        $data = $this->twig->render('{{ file | dataUri(false, "image/jpeg") }}', array('file' => $ressource));
+        $this->assertEquals('data:image/jpeg;base64,iVBORw0KGgoAAAANSUhEUgAAAB'
+            . 'AAAAAQCAMAAAAoLQ9TAAAAyVBMVEUzM2a9pUL90Bzi0phoZH3/6pb/9cytooiEg'
+            . '3v93FhST2vuwhL977qVjGr/++z/1zl0cILPvoj/5X9eW3Xix1/9ywP/7aX/+NyL'
+            . 'hnb/88Sfk37NwZ86OWb80ix0bHD/5HZZV3T/8LX//vn/20yFeWr/6I1jYob7zBD'
+            . 'GqTrp4MNoZ4j/7J7/9tPNtWCDg4P/4mtSUHT1xwuckWT//fP+2kfpz2r/5oReXY'
+            . 'D8zAr/7qz/+uSLhnaflYjPx7Q6Omn70zP977qAeHoAAAAdPTPOAAAAQ3RSTlP//'
+            . '///////////////////////////////////////////////////////////////'
+            . '//////////////////////8AQWIE7wAAAM5JREFUGFclj9lywjAMRcWSkIWGVTY'
+            . 'ppTQMe0JCwA22hzZT//9HVTb3SeeMNJLA2KCNqwxYlJVSeSidIoFCJ5oXqWqtAe'
+            . 'LN3YmMrWorpL7vH5r7/S/WyKMBrJLk/OCF35/luzYmwbSmAT/NlNcIXAAqzgvio'
+            . 'cp3a4FvgN4JfiyzCXyPbEeYXsfDTDHvMwq7cUlb2PL3+hy8Rx8XgR0S2LLeXwTR'
+            . 'di5kHbhLV00zXVehOOCtdL/U2Aox6mJN7IQ5xvRs3AmIX8KY4BaUFo35B3lHJ6U'
+            . 'KnYJ8AAAAAElFTkSuQmCC', $data);
+    }
+
+    /**
+     * @covers DataUri\TwigExtension::dataUri
+     * @covers DataUri\TwigExtension::getDataFromScalar
+     */
+    public function testDataUriScalar()
+    {
+        $data = $this->twig->render('{{ scalarValue | dataUri }}', array('scalarValue' => 'Hello world !'));
+        $this->assertEquals('data:text/plain;charset=US-ASCII,Hello%20world%20%21', $data);
+
+        $data = $this->twig->render('{{ scalarValue | dataUri(true, "text/csv", {"charset":"utf-8"}) }}', array('scalarValue' => 'Bonne année !'));
+        $this->assertEquals('data:text/csv;charset=utf-8,Bonne%20ann%C3%A9e%20%21', $data);
+    }
+
+    /**
+     * @covers DataUri\TwigExtension::dataUri
+     * @covers DataUri\TwigExtension::getDataFromScalar
+     */
+    public function testDataUriFile()
+    {
+        $filepath = __DIR__ . '/../../photo01.JPG';
+        $this->twig->render('{{ filepath | dataUri(false) }}', array('filepath' => $filepath));
+
+        $filepath = __DIR__ . '/../../smile.png';
+        $this->twig->render('{{ filepath | dataUri }}', array('filepath' => $filepath));
+    }
+
+    /**
+     * @covers DataUri\TwigExtension::dataUri
+     * @covers DataUri\TwigExtension::getDataFromScalar
+     */
+    public function testDataUriFileStrict()
+    {
+        $data = $this->twig->render('{{ file | dataUri(false) }}', array('file' => __DIR__ . '/../../photo01.JPG'));
+        $this->assertTrue(strlen($data) > 4000000);
+        $this->assertTrue(strpos($data, 'data:image/jpeg;base64,LzlqLzRTLytSWGhwWmdBQVRV') === 0);
+    }
+
+    /**
+     * @covers DataUri\TwigExtension::dataUri
+     * @expectedException \Twig_Error_Runtime
+     */
+    public function testDataUriUnknownFormat()
+    {
+        $this->twig->render('{{ array | dataUri(false) }}', array('array' => array()));
+    }
+}
